0465c7634e8458c46d8cde7114a70feb8d564d0e
Andrew Waite <apwaite86@gmail.com>
2021-02-19 01:08:43 +0000

    Created two functions (one using CSS grid and the other creating a grid using inline-block. Functions generate a grid of a set size

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-// Select elements
+const main = document.querySelector(".main");
 
-const grid = document.querySelector(".main");
+function makeGrid(rows, cols) {
+  main.style.setProperty("--grid-rows", rows);
+  main.style.setProperty("--grid-cols", cols);
+  for (let i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    cell.innerText = "";
+    main.appendChild(cell).className = "grid-item";
+  }
+}
 
-grid.textContet = "This is has been selected correctly";
+// function makeGrid(num) {
+//   const e = document.querySelector(".main");
+//   for (let i = 0; i < num; i++) {
+//     let row = document.createElement("div");
+//     row.className = "row";
+//     for (let j = 1; j <= num; j++) {
+//       let cell = document.createElement("div");
+//       cell.className = "cell";
+//       cell.innerText = i * num + j;
+//       row.appendChild(cell);
+//     }
+//     e.appendChild(row);
+//   }
+// }
