d11d14a5ba048d82e935e17fd5e2cedf3115ad56
Andrew Waite <apwaite86@gmail.com>
2021-02-22 00:42:15 +0000

    Refactored removeGrid function to use querySelectorAll to select all divs with the parent main instead of taking all the children nodes of main and placing them into an array. Refactored createNewGrid function to parseInt on the prompt and updated the alert to request a grid up to 100 instead of 64 to reflect previous changes. Added new functionality including: a clear function that reset the background color of all grid divs to their default color, a draw function that adds a mousover event listener to every div whose parent class is main so their background could be changed, a function to visually display whether standard or rainbow options had been selected, a randomColor function that would randomly generate an rgb color when executed and an event listener that would execute a loadGame function when the page finished loading

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ const main = document.querySelector(".main");
 
 const newGridButton = document.querySelector("#new-grid");
 
+const penColor = document.querySelectorAll(".penColor");
+
+const reset = document.querySelector("#reset");
+
+let currentColor = "standard";
+
+window.addEventListener("load", loadGame);
+
 newGridButton.addEventListener("click", createNewGrid);
 
-window.addEventListener("load", defaultSize);
+reset.addEventListener("click", () => {
+  clear();
+  draw(currentColor);
+});
 
 function defaultSize() {
   makeGrid(24);
 }
+
 function makeGrid(val) {
   let gridSize = val * val;
   main.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
@@ -19,22 +31,98 @@ function makeGrid(val) {
   }
 }
 
-function removeGrid() {
-  const mainArray = Array.from(main.childNodes);
+function clear() {
+  const gridItems = document.querySelectorAll(".main > div");
 
-  mainArray.forEach((e) => {
-    main.removeChild(e);
+  gridItems.forEach((item) => {
+    item.style = "background-color = rgb(255, 255, 255)";
   });
+
+  // const gridItems = Array.from(main.childNodes);
+
+  // gridItems.forEach((items) => {
+  //   items.style = "background-color = rgb(255, 255, 255)";
+  // });
+}
+
+function removeGrid() {
+  const gridItems = document.querySelectorAll(".main > div");
+
+  gridItems.forEach((items) => {
+    main.removeChild(items);
+  });
+
+  // const gridItems = Array.from(main.childNodes);
+
+  // gridItems.forEach((items) => {
+  //   main.removeChild(items);
+  // });
 }
 
 function createNewGrid() {
-  let newGrid = prompt("Please enter a value between 1 and 64");
+  const newGrid = parseInt(prompt("Please enter a value between 1 and 100: "));
 
   if (newGrid < 1 || newGrid > 100 || isNaN(newGrid)) {
-    alert("Error! Please enter a number between 1 and 64.");
+    alert("Error! Please enter a number between 1 and 100.");
     createNewGrid();
   } else {
     removeGrid();
     makeGrid(newGrid);
+    draw(currentColor);
   }
+}
+
+function draw(color) {
+  const gridItems = document.querySelectorAll(".main > div");
+
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+      if (color === "standard" || currentColor === "standard") {
+        e.target.style = "background-color: rgba(46, 49, 49)";
+      } else if (color === "rainbow") {
+        e.target.style.backgroundColor = randomColor();
+      }
+    });
+  });
+}
+
+function selectButton(button) {
+  if (button.classList.contains("penColor")) {
+    penColor.forEach((selection) => {
+      selection.classList.remove("active");
+    });
+  }
+  button.classList.add("active");
+}
+
+function changeColor() {
+  penColor[0].classList.add("active");
+
+  penColor.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("standard")) {
+        draw("standard");
+        currentColor = "standard";
+        selectButton(item);
+      } else if (item.classList.contains("rainbow")) {
+        draw("rainbow");
+        currentColor = "rainbow";
+        selectButton(item);
+      }
+    });
+  });
+}
+
+function randomColor() {
+  const r1 = Math.floor(Math.random() * 256);
+  const r2 = Math.floor(Math.random() * 256);
+  const r3 = Math.floor(Math.random() * 256);
+  const bgColor = `rgb(${r1}, ${r2}, ${r3})`;
+  return bgColor;
+}
+
+function loadGame() {
+  defaultSize();
+  draw("standard");
+  changeColor();
 }
